1f115f4bf864a5e4180aa1eb3f53cd184404598d
Jimmy <jshen35@gmail.com>
2020-12-20 14:27:31 -0500

    Load Code Feature

--- app/desktop/src/view/main/MainViewController.js
+++ app/desktop/src/view/main/MainViewController.js
@@ -1,94 +1,150 @@
 Ext.define('pso2affixsim.view.main.MainViewController', {
-  extend: 'Ext.app.ViewController',
-  alias: 'controller.mainviewcontroller',
-  requires: ['Ext.ux.Mediator'],
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.mainviewcontroller',
+    requires: ['Ext.ux.Mediator'],
 
-  initViewModel: function (vm) {
-  },
-  campaignBoostChangeEvent: function (field, newValue, oldValue, opts) {
-    Ext.ux.Mediator.fireEvent('campaignChange', newValue);
-  },
-  groupTypeBoostChangeEvent: function (field, newValue, oldValue, opts) {
-    Ext.ux.Mediator.fireEvent('groupTypeChange', newValue);
-  },
-  groupvalueBoostChangeEvent: function (field, newValue, oldValue, opts) {
-    Ext.ux.Mediator.fireEvent('groupValueChange', newValue);
-  },
-  changeBaseColor: function (field, color, previousColor, eOpts) {
-    if (previousColor != null) {
-      var storage = Ext.util.LocalStorage.get("affixsim");
-      if (storage) {
-        storage.setItem("base-color", color)
-      }
-      Fashion.css.setVariables(
-        {
-          "base-color": '#' + color,
-          "dark-mode": this.getViewModel().get('dark_mode').toString()
+    initViewModel: function(vm) {},
+    campaignBoostChangeEvent: function(field, newValue, oldValue, opts) {
+        Ext.ux.Mediator.fireEvent('campaignChange', newValue);
+    },
+    groupTypeBoostChangeEvent: function(field, newValue, oldValue, opts) {
+        Ext.ux.Mediator.fireEvent('groupTypeChange', newValue);
+    },
+    groupvalueBoostChangeEvent: function(field, newValue, oldValue, opts) {
+        Ext.ux.Mediator.fireEvent('groupValueChange', newValue);
+    },
+    changeBaseColor: function(field, color, previousColor, eOpts) {
+        if (previousColor != null) {
+            var storage = Ext.util.LocalStorage.get("affixsim");
+            if (storage) {
+                storage.setItem("base-color", color)
+            }
+            Fashion.css.setVariables({
+                "base-color": '#' + color,
+                "dark-mode": this.getViewModel().get('dark_mode').toString()
+            });
         }
-      );
-    }
-  },
-  changeDarkMode: function (checkbox, newValue, oldValue, eOpts) {
-    this.getViewModel().set('dark_mode', newValue);
-    var storage = Ext.util.LocalStorage.get('affixsim');
-    if (storage) {
-      storage.setItem("dark-mode", newValue)
-    }
-    Fashion.css.setVariables(
-      {
-        "base-color": '#' + this.getViewModel().get('color'),
-        "dark-mode": newValue.toString()
-      });
-  },
-  changeServer: function (field, newValue, oldValue, eOpts) {
-    if (oldValue != null) {
-      var storage = Ext.util.LocalStorage.get("affixsim");
-      if (storage) {
-        storage.setItem("server", newValue)
-      }
-    }
-  },
-  changeLanguage: function (field, newValue, oldValue, eOpts) {
-    if (oldValue != null) {
-      var storage = Ext.util.LocalStorage.get("affixsim");
-      if (storage) {
-        storage.setItem("language", newValue)
-      }
-    }
-  },
-  onAddTabClick: function () {
-    var tabPanel = this.lookupReference('tabpanel'),
-      tab = tabPanel.add({
-        xtype: "tabview"
-      });
+    },
+    changeDarkMode: function(checkbox, newValue, oldValue, eOpts) {
+        this.getViewModel().set('dark_mode', newValue);
+        var storage = Ext.util.LocalStorage.get('affixsim');
+        if (storage) {
+            storage.setItem("dark-mode", newValue)
+        }
+        Fashion.css.setVariables({
+            "base-color": '#' + this.getViewModel().get('color'),
+            "dark-mode": newValue.toString()
+        });
+    },
+    changeServer: function(field, newValue, oldValue, eOpts) {
+        if (oldValue != null) {
+            var storage = Ext.util.LocalStorage.get("affixsim");
+            if (storage) {
+                storage.setItem("server", newValue)
+            }
+        }
+    },
+    changeLanguage: function(field, newValue, oldValue, eOpts) {
+        if (oldValue != null) {
+            var storage = Ext.util.LocalStorage.get("affixsim");
+            if (storage) {
+                storage.setItem("language", newValue)
+            }
+        }
+    },
+    onAddTabClick: function() {
+        var tabPanel = this.lookupReference('tabpanel'),
+            tab = tabPanel.add({
+                xtype: "tabview"
+            });
 
-    tabPanel.setActiveTab(tab);
-    var test = JSON.parse('{"affixes":[["AA01"],["AA03"],[],[],[],[]],"saf":[null,"AA04"],"selection":["IT03"],"itemboost":"A04","potential":"C12","additem":"IT03","same":true}')
-    tab.loadTabData(test)
-  },
-  makeActiveTabValid: function () {
-    var tabPanel = this.lookupReference('tabpanel'),
-      tab = tabPanel.getActiveTab(tab)
-    if (tab != null) tab.makeTabValid()
-  },
-  renameActiveTab: function () {
-    var tabPanel = this.lookupReference('tabpanel'),
-      tab = tabPanel.getActiveTab(tab)
-    if (tab != null){
-      return Ext.Msg.prompt("Rename Panel", "Rename current panel.<br/>Input a name?", function(okButton, textfield) {
-        if (okButton == "ok") {
-            tab.rename(textfield)
+        tabPanel.setActiveTab(tab);
+    },
+    makeActiveTabValid: function() {
+        var tabPanel = this.lookupReference('tabpanel'),
+            tab = tabPanel.getActiveTab(tab)
+        if (tab != null) tab.makeTabValid()
+    },
+    renameActiveTab: function() {
+        var tabPanel = this.lookupReference('tabpanel'),
+            tab = tabPanel.getActiveTab(tab)
+        if (tab != null) {
+            return Ext.Msg.prompt("Rename Panel", "Rename current panel.<br/>Input a name?", function(okButton, textfield) {
+                if (okButton == "ok") {
+                    tab.rename(textfield)
+                }
+            })
         }
-      })
-    } 
-  },
-  getActiveTabData: function(){
-    var tabPanel = this.lookupReference('tabpanel'),
-    tab = tabPanel.getActiveTab(tab)
-    console.log(tab.getTabData())
-  },
-  onHeaderViewDetailToggle: function () {
-    var vm = this.getViewModel();
-    vm.set('detailCollapsed', !vm.get('detailCollapsed'));
-  }
+    },
+    loadExistingPlan: function() {
+        var tabPanel = this.lookupReference('tabpanel')
+        var win = Ext.create("widget.window", {
+            title: "Load Plan",
+            autoDestroy: true,
+            closable: true,
+            width: 600,
+            autoHeight: true,
+            modal: true,
+            layout: "fit",
+            margin: "10 10 10 10",
+            bodyStyle: {
+                padding: "10px"
+            },
+            items: [{
+                xtype: 'textfield',
+                name: 'code',
+                fieldLabel: 'Code:'
+            }],
+            dockedItems: [{
+                xtype: "toolbar",
+                ui: "footer",
+                dock: "bottom",
+                items: [{
+                        xtype: "label",
+                        name: "error",
+                        readOnly: true,
+                        textAlign: "right",
+                        html: "&nbsp",
+                        bodyStyle: {
+                            "float": "hidden"
+                        }
+                    }, "->",
+                    Ext.create("Ext.button.Button", {
+                        text: "Load",
+                        handler: function() {
+                            var json, code = win.down('textfield[name=code]').getValue().trim()
+                            try {
+                                if (!code) throw "Invalid"
+                                json = JSON.parse(LZString.decompressFromEncodedURIComponent(code))
+                            } catch (e) {
+                                win.down('label[name=error]').setHtml('<div style="color:red">Invalid code</div>')
+                                return
+                            }
+                            var tab = tabPanel.add({
+                                xtype: "tabview"
+                            });
+                            tab.loadTabData(json)
+                            tabPanel.setActiveTab(tab);
+                        },
+                        minWidth: 64
+                    }), Ext.create("Ext.button.Button", {
+                        text: "Close",
+                        handler: function() {
+                            win.hide()
+                        },
+                        minWidth: 64
+                    })
+                ]
+            }]
+        }).show()
+    },
+    getActiveTabData: function() {
+        var tabPanel = this.lookupReference('tabpanel'),
+            tab = tabPanel.getActiveTab(tab)
+        console.log(tab.getTabData())
+    },
+    onHeaderViewDetailToggle: function() {
+        var vm = this.getViewModel();
+        vm.set('detailCollapsed', !vm.get('detailCollapsed'));
+    }
 });
--- app/desktop/src/view/main/center/CenterView.js
+++ app/desktop/src/view/main/center/CenterView.js
@@ -1,157 +1,152 @@
 Ext.define('pso2affixsim.view.main.center.CenterView', {
-	extend: 'Ext.panel.Panel',
-	xtype: 'centerview',
-	cls: 'centerview',
-	layout: 'card',
-	padding: 5,
-	tbar: [
-		{
-			xtype: 'button',
-			icon: null,
-			text: "Add Panel",
-			iconCls: 'x-fa fa-plus',
-			listeners: {
-				click: 'onAddTabClick'
-			},
-			tooltip: "Add new panel"
-		},
-		{
-			xtype: 'button',
-			icon: null,
-			text: "Rename Tab",
-			iconCls: 'x-fa fa-edit',
-			listeners: {
-				click: 'renameActiveTab'
-			},
-			tooltip: "Rename current tab"
-		},
-		{
-			xtype: 'button',
-			icon: null,
-			text: "Make Active Tab Valid",
-			iconCls: 'x-fa fa-check',
-			listeners: {
-				click: 'makeActiveTabValid'
-			},
-			tooltip: "Add fillers to make active tab valid"
-		},
-		{
-			xtype: 'button',
-			icon: null,
-			text: "Save",
-			iconCls: 'x-fa fa-save',
-			listeners: {
-				click: 'getActiveTabData'
-			},
-			style: {
-				pointerEvents: 'all'
-			},
-			tooltip: "Not available"
-		},
-		{
-			xtype: 'button',
-			icon: null,
-			text: "Load",
-			iconCls: 'x-fa fa-download',
-			style: {
-				pointerEvents: 'all'
-			},
-			tooltip: "Not available",
-			disabled: true
-		},
-		'-',
-		{
-			xtype: 'numberfield',
-			name: 'campaignBoost',
-			fieldLabel: 'Campaign Boost',
-			value: 0,
-			maxValue: 100,
-			minValue: 0,
-			hideTrigger: true,
-			listeners: {
-				change: 'campaignBoostChangeEvent'
-			},
-			validator: function (val) {
-				var value = val;
-				if (value < 0) {
-					value = 0
-				} else if (value > 100) {
-					value = 100
-				}
-				this.setValue(value);
-				return true
-			}
-		},
-		'-',
-		{
-			xtype: 'combobox',
-			name: 'groupBoostType',
-			fieldLabel: 'Group Boost',
-			displayField: 'text',
-			forceSelection: true,
-			editable: false,
-			valueField: 'value',
-			value: 0,
-			queryMode: 'local',
-			listeners: {
-				change: 'groupTypeBoostChangeEvent'
-			},
-			store: Ext.create("Ext.data.Store", {
-				fields: ["text", "value"],
-				data: [
-					{
-						text: 'No Boost',
-						value: 0
-					},
-					{
-						text: 'Power',
-						value: 'Power'
-					},
-					{
-						text: 'Shoot',
-						value: 'Shoot'
-					},
-					{
-						text: 'Tech',
-						value: 'Tech'
-					},
-					{
-						text: 'HP & PP',
-						value: 'HPPP'
-					},
-					{
-						text: 'Special',
-						value: 'Special'
-					}
-				]
-			})
-		},
-		{
-			xtype: 'numberfield',
-			name: 'groupBoostNumber',
-			fieldLabel: ' ',
-			value: 0,
-			maxValue: 100,
-			minValue: 0,
-			hideTrigger: true,
-			listeners: {
-				change: 'groupvalueBoostChangeEvent'
-			},
-			validator: function (val) {
-				var value = val;
-				if (value < 0) {
-					value = 0
-				} else if (value > 100) {
-					value = 100
-				}
-				this.setValue(value);
-				return true
-			}
-		}
-	],
-	items: [
-		{
-			xtype: "tabpanelview"
-		}
-	]
+    extend: 'Ext.panel.Panel',
+    xtype: 'centerview',
+    cls: 'centerview',
+    layout: 'card',
+    padding: 5,
+    tbar: [{
+            xtype: 'button',
+            icon: null,
+            text: "Add Panel",
+            iconCls: 'x-fa fa-plus',
+            listeners: {
+                click: 'onAddTabClick'
+            },
+            tooltip: "Add new panel"
+        },
+        {
+            xtype: 'button',
+            icon: null,
+            text: "Rename Tab",
+            iconCls: 'x-fa fa-edit',
+            listeners: {
+                click: 'renameActiveTab'
+            },
+            tooltip: "Rename current tab"
+        },
+        {
+            xtype: 'button',
+            icon: null,
+            text: "Make Active Tab Valid",
+            iconCls: 'x-fa fa-check',
+            listeners: {
+                click: 'makeActiveTabValid'
+            },
+            tooltip: "Add fillers to make active tab valid"
+        },
+        {
+            xtype: 'button',
+            icon: null,
+            text: "Save",
+            iconCls: 'x-fa fa-save',
+            //listeners: {
+            //    click: 'getActiveTabData'
+            //},
+            style: {
+                pointerEvents: 'all'
+            },
+            tooltip: "Not available",
+            disabled: true
+        },
+        {
+            xtype: 'button',
+            icon: null,
+            text: "Load",
+            iconCls: 'x-fa fa-download',
+            listeners: {
+                click: 'loadExistingPlan'
+            }
+        },
+        '-',
+        {
+            xtype: 'numberfield',
+            name: 'campaignBoost',
+            fieldLabel: 'Campaign Boost',
+            value: 0,
+            maxValue: 100,
+            minValue: 0,
+            hideTrigger: true,
+            listeners: {
+                change: 'campaignBoostChangeEvent'
+            },
+            validator: function(val) {
+                var value = val;
+                if (value < 0) {
+                    value = 0
+                } else if (value > 100) {
+                    value = 100
+                }
+                this.setValue(value);
+                return true
+            }
+        },
+        '-',
+        {
+            xtype: 'combobox',
+            name: 'groupBoostType',
+            fieldLabel: 'Group Boost',
+            displayField: 'text',
+            forceSelection: true,
+            editable: false,
+            valueField: 'value',
+            value: 0,
+            queryMode: 'local',
+            listeners: {
+                change: 'groupTypeBoostChangeEvent'
+            },
+            store: Ext.create("Ext.data.Store", {
+                fields: ["text", "value"],
+                data: [{
+                        text: 'No Boost',
+                        value: 0
+                    },
+                    {
+                        text: 'Power',
+                        value: 'Power'
+                    },
+                    {
+                        text: 'Shoot',
+                        value: 'Shoot'
+                    },
+                    {
+                        text: 'Tech',
+                        value: 'Tech'
+                    },
+                    {
+                        text: 'HP & PP',
+                        value: 'HPPP'
+                    },
+                    {
+                        text: 'Special',
+                        value: 'Special'
+                    }
+                ]
+            })
+        },
+        {
+            xtype: 'numberfield',
+            name: 'groupBoostNumber',
+            fieldLabel: ' ',
+            value: 0,
+            maxValue: 100,
+            minValue: 0,
+            hideTrigger: true,
+            listeners: {
+                change: 'groupvalueBoostChangeEvent'
+            },
+            validator: function(val) {
+                var value = val;
+                if (value < 0) {
+                    value = 0
+                } else if (value > 100) {
+                    value = 100
+                }
+                this.setValue(value);
+                return true
+            }
+        }
+    ],
+    items: [{
+        xtype: "tabpanelview"
+    }]
 });
